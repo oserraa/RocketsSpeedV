6d8c9e5f47b213d37a0da9d3c47e05e7839ea9e7
williamyangT <wyang@edu.tecnocampus.cat>
2020-05-08 12:42:52 +0200

    proppp

--- Rockets Speedy V/src/CapaDomini/Propeller.java
+++ Rockets Speedy V/src/CapaDomini/Propeller.java
@@ -1,29 +1,6 @@
 
 package CapaDomini;
 
-public class Propeller {
-
-		private int maxAcceleration;
-		
-		public void getAcceleration() {
-			
-		}
-		
-		public int accelerate() {
-			return 0;
-		}
-		
-		public int brake() {
-			return 0;
-		}
-		
-		public void keepAcceleration() {
-			
-	}
-
-}
-
-package CapaDomini;
 
 public class Propeller {
 	private int maxAcceleration;
