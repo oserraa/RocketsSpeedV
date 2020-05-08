f88150a72ecdb3de705c85b169f7f0a1c2e5009f
oserraa <oserraa@edu.tecnocampus.cat>
2020-05-08 11:54:51 +0200

    Rocket finishing

--- Rockets Speedy V/src/CapaDomini/Rocket.java
+++ Rockets Speedy V/src/CapaDomini/Rocket.java
@@ -1,6 +1,27 @@
 package CapaDomini;
 
 public class Rocket {
-	int i;
+	String Name;
+	Double currentSpeed;
+	int currentAcceleration;
+	Double metersTravelled;
+	int circuitPosition;
+	
+	public Rocket(String name,int propellers) {
+		
+	}
+	
+	public void updateData(int time) {
+		this.metersTravelled=(0+currentSpeed*time+0.5*currentAcceleration*(time^2));
+		this.currentSpeed=currentSpeed+currentAcceleration*time;
+	}
+	public Double getSpeed() {return currentSpeed;}
+	public Double getMetersTravelled() {return metersTravelled;}
+	//public getFuel() {}
+	public void setPosition(int position) {
+		circuitPosition=position;
+	}
+	public int getPosition() {return circuitPosition;}
+		
 		
 }
