e3daf7251bc3639be3e5db0ea7d623566d060695
williamyangT <wyang@edu.tecnocampus.cat>
2020-06-11 14:28:08 +0200

    correcciones

--- Rockets Speedy V/src/Domain/Rocket.java
+++ Rockets Speedy V/src/Domain/Rocket.java
@@ -1,4 +1,5 @@
 package Domain;
+
 import java.util.*;
 
 import java.util.ArrayList;
@@ -6,74 +7,105 @@ import java.util.List;
 
 public class Rocket {
 	private String name;
-	private double currentSpeed=0;
-	private int currentAcceleration=0;
-	private double metersTravelled=0;
+	private double currentSpeed = 0;
+	private int currentAcceleration = 0;
+	private double metersTravelled = 0;
 	private int circuitPosition;
 	private FuelTank fuelTank;
 
-	private List<Propeller> propellers=new ArrayList<Propeller>();
-	
-	public Rocket(String name,List<Integer>propellers,int fuel) {
-		//validar dades
-		this.name=name;
-		fuelTank=new FuelTank(fuel);
-		for(Integer propeller: propellers) {
+	private List<Propeller> propellers = new ArrayList<Propeller>();
+
+	public Rocket(String name, List<Integer> propellers, int fuel) {
+		// validar dades
+		this.name = name;
+		fuelTank = new FuelTank(fuel);
+		for (Integer propeller : propellers) {
 			this.propellers.add(new Propeller(propeller));
 		}
 	}
-	
+
 	public void BackUpdateData(int acce) {
 		setAcceleration(acce);
-		currentAcceleration=this.getAcceleration();		
+		currentAcceleration = this.getAcceleration();
 		updateData(1);
-		fuelTank.updateFuel(currentSpeed);	
-		
+		fuelTank.updateFuel(currentSpeed);
+
 	}
+
 	public void askMovement(int time) {
 		setAcceleration(Strategy.getInstance(this).accelerationOnTime(time));
-		currentAcceleration=this.getAcceleration();		
+		currentAcceleration = this.getAcceleration();
 		updateData(1);
-		fuelTank.updateFuel(currentSpeed);	
-		
+		fuelTank.updateFuel(currentSpeed);
+
 	}
+
 	public void setAcceleration(int acceleration) {
-		for(Propeller propeller:propellers) {
+		for (Propeller propeller : propellers) {
 			propeller.accelerate(acceleration);
 		}
 	}
+
 	public int getAcceleration() {
-		int acceleration=0;
-		for(Propeller propeller:propellers) {
-			acceleration+=propeller.getCurrentAcceleration();
+		int acceleration = 0;
+		for (Propeller propeller : propellers) {
+			acceleration += propeller.getCurrentAcceleration();
 		}
 		return acceleration;
 	}
-	
+
 	public void updateData(int time) {
-		this.metersTravelled+=(0+currentSpeed*time+0.5*currentAcceleration*(time^2));
-		if(this.fuelTank.getCurrentFuel()==0) {
-			this.currentSpeed=0;
-		}
-		else {
-			this.currentSpeed=currentSpeed+currentAcceleration*time;
+		this.metersTravelled += (0 + currentSpeed * time + 0.5 * currentAcceleration * (time ^ 2));
+		if (this.fuelTank.getCurrentFuel() == 0) {
+			this.currentSpeed = 0;
+		} else {
+			this.currentSpeed = currentSpeed + currentAcceleration * time;
 		}
 	}
-	
-	public String getName() {return name;}
-	public Double getSpeed() {return currentSpeed;}
-	public int getCurrentAcceleration() {return currentAcceleration;}
-	public Double getMetersTravelled() {return metersTravelled;}
-	/*public void setPosition(int position) {
-		circuitPosition=position;
-	}*/
-	public int getPosition() {return circuitPosition;}
-	public int getCurrentFuel() {return fuelTank.getCurrentFuel();}
-	public int getMaxFuel() {return fuelTank.getMaxFuel();}
-	public List<Propeller> getPropellers(){return propellers;}
-	
-	public void setSpeed(double speed) {this.currentSpeed=speed;}
-	public void setMetersTravelled(double meters) {this.metersTravelled=meters;}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getSpeed() {
+		return currentSpeed;
+	}
+
+	public int getCurrentAcceleration() {
+		return currentAcceleration;
+	}
+
+	public Double getMetersTravelled() {
+		return metersTravelled;
+	}
+
+	/*
+	 * public void setPosition(int position) { circuitPosition=position; }
+	 */
+	public int getPosition() {
+		return circuitPosition;
+	}
+
+	public int getCurrentFuel() {
+		return fuelTank.getCurrentFuel();
+	}
+
+	public int getMaxFuel() {
+		return fuelTank.getMaxFuel();
+	}
+
+	public List<Propeller> getPropellers() {
+		return propellers;
+	}
+
+	public void setSpeed(double speed) {
+		this.currentSpeed = speed;
+	}
+
+	public void setMetersTravelled(double meters) {
+		this.metersTravelled = meters;
+	}
+
 	public void setCurrentFuel(int fuel) {
 		this.fuelTank.setCurrentFuel(fuel);
 	}
--- Rockets Speedy V/src/Domain/Strategy.java
+++ Rockets Speedy V/src/Domain/Strategy.java
@@ -1,118 +1,119 @@
 package Domain;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Strategy {
-	
+
 	private static Strategy instance;
-	private int[]numbers=new int[28];
-	private int meters=1700;
+	private int[] numbers = new int[28];
+	private int meters = 1700;
 	private Rocket rocket;
-	
-	private List<Integer> bestWay=new ArrayList<Integer>();
-	
+
+	private List<Integer> bestWay = new ArrayList<Integer>();
+
 	public synchronized static Strategy getInstance(Rocket r) {
-		if(instance==null) {
-			instance= new Strategy(r);
+		if (instance == null) {
+			instance = new Strategy(r);
 		}
 		return instance;
 	}
-	
+
 	private Strategy(Rocket rocket) {
-		this.rocket=rocket;
-		List<Integer> sol=new ArrayList<Integer>();
-		backBestRoute(sol,1,bestWay);
+		this.rocket = rocket;
+		List<Integer> sol = new ArrayList<Integer>();
+		backBestRoute(sol, 1, bestWay);
 	}
-	
+
 	public int accelerationOnTime(int time) {
 		try {
-			return bestWay.get(time-1);
-		}catch(Exception e) {
+			return bestWay.get(time - 1);
+		} catch (Exception e) {
 			return 0;
-		}	
+		}
 	}
-	
+
 	public void backBestRoute(List<Integer> solution, int k, List<Integer> best) {
-		int i=maxAcceleration();
-		double speedbefore=0;
-		double metersbefore=0;
-		int fuelbefore=0;
-		while(i>=0) {
-			if(acceptable(k,i)) {
-				metersbefore=rocket.getMetersTravelled();
-				speedbefore=rocket.getSpeed();
-				fuelbefore=rocket.getCurrentFuel();
+		int i = maxAcceleration();
+		double speedbefore = 0;
+		double metersbefore = 0;
+		int fuelbefore = 0;
+		while (i >= 0) {
+			if (acceptable(k, i)) {
+				metersbefore = rocket.getMetersTravelled();
+				speedbefore = rocket.getSpeed();
+				fuelbefore = rocket.getCurrentFuel();
 				solution.add(i);
 				rocket.BackUpdateData(i);
-				if(esSolucio()) {
-					if(millorSolucio(solution,best)) {
+				if (esSolucio()) {
+					if (millorSolucio(solution, best)) {
 						best.clear();
-						for(int newAcceleration:solution) {
+						for (int newAcceleration : solution) {
 							best.add(newAcceleration);
 						}
 					}
-				}else if(!esSolucio()) {
-					backBestRoute(solution,k+1,best);
+				} else if (!esSolucio()) {
+					backBestRoute(solution, k + 1, best);
 				}
-				solution.remove(k-1);
+				solution.remove(k - 1);
 				remove(speedbefore, metersbefore, fuelbefore);
-				
 
 			}
 			i--;
 		}
 	}
-	
+
 	private void remove(double speedbefore, double metersbefore, int fuelbefore) {
 		rocket.setSpeed(speedbefore);
 		rocket.setMetersTravelled(metersbefore);
 		rocket.setCurrentFuel(fuelbefore);
 	}
-	
-	private boolean acceptable(int k,int acceleration) {
-		if(k<=numbers.length) {
-			if(calcularGasolina(k,acceleration)>=0) {
+
+	private boolean acceptable(int k, int acceleration) {
+		if (k <= numbers.length) {
+			if (calcularGasolina(k, acceleration) >= 0) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	private int maxAcceleration() {
-		int maxAcceleration=0;
-		for(Propeller propeller: rocket.getPropellers()) {
-			if(maxAcceleration<propeller.getMaxAcceleration()) {
-				maxAcceleration=propeller.getMaxAcceleration();
+		int maxAcceleration = 0;
+		for (Propeller propeller : rocket.getPropellers()) {
+			if (maxAcceleration < propeller.getMaxAcceleration()) {
+				maxAcceleration = propeller.getMaxAcceleration();
 			}
 		}
-		return maxAcceleration;			
+		return maxAcceleration;
 	}
-	private int calcularGasolina(int k,int acceleration) {
-		double speed=rocket.getSpeed()+calcularAcceleration(acceleration);
-		int fuel=rocket.getCurrentFuel()-(int) Math.round(0.02*(speed*speed));
+
+	private int calcularGasolina(int k, int acceleration) {
+		double speed = rocket.getSpeed() + calcularAcceleration(acceleration);
+		int fuel = rocket.getCurrentFuel() - (int) Math.round(0.02 * (speed * speed));
 		return fuel;
 	}
+
 	private int calcularAcceleration(int i) {
-		int acc=0;
-		for(int j=0;j<rocket.getPropellers().size();j++) {
-			if(i>rocket.getPropellers().get(j).getMaxAcceleration()) {
-				acc+=rocket.getPropellers().get(j).getMaxAcceleration();
+		int acc = 0;
+		for (int j = 0; j < rocket.getPropellers().size(); j++) {
+			if (i > rocket.getPropellers().get(j).getMaxAcceleration()) {
+				acc += rocket.getPropellers().get(j).getMaxAcceleration();
+			} else {
+				acc += i;
 			}
-			else {
-				acc+=i;
-			}	
 		}
-		return acc;		
+		return acc;
 	}
-	
+
 	private boolean esSolucio() {
-		return rocket.getMetersTravelled()>=this.meters;
+		return rocket.getMetersTravelled() >= this.meters;
 	}
-	
-	private boolean millorSolucio(List<Integer> sol,List<Integer> best) {
-		if(best.size()==0)return true;
-		return sol.size()<best.size();
+
+	private boolean millorSolucio(List<Integer> sol, List<Integer> best) {
+		if (best.size() == 0)
+			return true;
+		return sol.size() < best.size();
 	}
-	
 
 }
