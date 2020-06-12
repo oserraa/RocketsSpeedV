6b3eb666ef4edc8280b2a9b61738631383596a6a
oserraa <oserraa@edu.tecnocampus.cat>
2020-06-12 13:58:18 +0200

    Finished fase 2

--- Rockets Speedy V/src/View/Main.java
+++ Rockets Speedy V/src/View/Main.java
@@ -10,37 +10,51 @@ public class Main {
 	
 	public static void main (String [] args) throws Exception{
 		Controller controller= new Controller();
+		String solution="";
 		System.out.println("Starting the race. Make your bets while rockets are being prepared...");
 		System.out.println(controller.play());
-		System.out.println(controller.competitionInProgress());
+		solution=controller.competitionInProgress();
+		int n=0;
+		for(int x=0;x<solution.length();x++) {
+			if(solution.charAt(x)==('\n')) {
+				n++;
+			}
+			if(n==5) {
+				n=0;
+				Thread.sleep(1000);
+			}
+			System.out.print(solution.charAt(x));
+		}	
+		
+		System.out.println();
 		controller.registerRecord();
 		RecordDTO record;
 		try {
 			record=getRecord(controller.getCircuitName());
 		}catch(Exception e) {
 			record=createRecord(controller.getNameWinner(),controller.getTimeWinner(),controller.getCircuitName());
+			System.out.println("Winner has been saved to the Data Base");
 		}
 		if(controller.getTimeWinner()<record.getTime()) {
 			updateRecord(controller.getNameWinner(),controller.getTimeWinner(),controller.getCircuitName());
+			System.out.println("New RECORD!!!");
 		}
+		
 	}
 	
 	private static void updateRecord(String RocketName,int RocketTime,String CircuitName) throws Exception {
 		RecordDTO record = new RecordDTO(RocketName,RocketTime,CircuitName);
 		record= recordController.updateRecord(CircuitName, record);
-		System.out.print(record);
 	}
 	
 	private static RecordDTO getRecord(String CircuitName) throws Exception{
 		RecordDTO record= recordController.getRecord(CircuitName);
-	    System.out.println(record);
 	    return record;
 	}
 	
 	private static RecordDTO createRecord(String RocketName,int RocketTime,String CircuitName)throws Exception {
 		RecordDTO record = new RecordDTO(RocketName,RocketTime,CircuitName);
 		record=recordController.createRecord(record);
-		System.out.print(record);
 		return record;
 	}
 	
